77b4425d323aebbae1bfaf136186c0d0024c0be7
DrugTank <108267490+DrugTank@users.noreply.github.com>
2023-02-19 00:06:25 +0900

    Plates Counter Updated
    Title is description

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int platesSpawnedAmountMax = 4;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+
+        if (spawnPlateTimer > spawnPlateTimerMax)
+        {
+            spawnPlateTimer = 0;
+
+            if (platesSpawnedAmount < platesSpawnedAmountMax) 
+            {
+                platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {   // 플레이어가 가진게 없다면
+            if (platesSpawnedAmount > 0)
+            {
+                platesSpawnedAmount--;
+                // Interact 할 때에 생성해서 쥐어 줌
+                KitchenObject.SpawnKitchenObejct(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
